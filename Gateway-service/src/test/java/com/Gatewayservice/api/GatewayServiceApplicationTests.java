fb7ed70925f7fef4067a283941255f2c3288552a
devdattakhoche <49261633+devdattakhoche@users.noreply.github.com>
2019-12-28 15:44:19 +0530

    Add files via upload

---
+++ Gateway-service/src/test/java/com/Gatewayservice/api/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gatewayservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
